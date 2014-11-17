3bad44e3d200d6b325351096ebbdeb566330863a
jonathan_sterling <jonathan.sterling@jagex.com>
2014-11-17 10:28:48 +0000

    Moved MovingAveragesTest inside of MovingAverages so it's on the same git repo

---
+++ MovingAveragesTest/movingaveragestest/DatasetTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MovingAverages;
+
+namespace MovingAveragesTest
+{
+    [TestClass]
+    public class DatasetTests
+    {
+        [TestMethod]
+        public void LoadCountTest()
+        {
+            Dataset dataset = new Dataset();
+            dataset.load("MovingAveragesData.csv");            
+
+            Assert.AreEqual(2843, dataset.getEntries().Count );
+        }
+
+        [TestMethod]
+        public void FirstEntryPriceTest()
+        {
+            Dataset dataset = new Dataset();
+            dataset.load("MovingAveragesData.csv");
+
+            Assert.AreEqual(1.33877M, dataset.getEntries().First().Price);
+        }
+
+        [TestMethod]
+        public void ShowAllEntriesTest()
+        {
+            Dataset dataset = new Dataset();
+            dataset.load("MovingAveragesData.csv");
+
+            foreach( Entry entry in dataset.getEntries() )
+            {
+                Console.WriteLine(entry);
+            }
+        }
+
+        [TestMethod]
+        public void TestIndexLookup()
+        {
+            Dataset dataset = new Dataset();
+            dataset.load("MovingAveragesData.csv");
+
+            // Invalid range (before any valid data)
+            Assert.AreEqual(-1, dataset.getIndexGivenDateTime(new DateTime(2013, 08, 01, 00, 01, 00)));
+
+            // Beginning
+            Assert.AreEqual(0, dataset.getIndexGivenDateTime(new DateTime(2014, 08, 01, 00, 01, 00)));
+            Assert.AreEqual(1, dataset.getIndexGivenDateTime(new DateTime(2014, 08, 01, 00, 02, 00)));
+            Assert.AreEqual(2, dataset.getIndexGivenDateTime(new DateTime(2014, 08, 01, 00, 03, 00)));
+
+            // Middleish
+            Assert.AreEqual(1340, dataset.getIndexGivenDateTime(new DateTime(2014, 08, 03, 22, 30, 00)));
+
+            //End
+            Assert.AreEqual(2841, dataset.getIndexGivenDateTime(new DateTime(2014, 08, 04, 23, 59, 00)));
+            Assert.AreEqual(2842, dataset.getIndexGivenDateTime(new DateTime(2014, 08, 05, 00, 00, 00)));
+
+            // Invalid range (after any valid data)
+            Assert.AreEqual(-1, dataset.getIndexGivenDateTime(new DateTime(2015, 08, 01, 00, 01, 00)));
+        }
+    }    
+}
---
+++ MovingAveragesTest/movingaveragestest/ReliableCalculationTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MovingAverages;
+using System.Collections.Generic;
+
+namespace MovingAveragesTest
+{
+    [TestClass]
+    public class ReliableCalculationTests
+    {
+        protected Dataset reliableDataset;
+
+        [TestInitialize]
+        public void initialise()
+        {
+            reliableDataset = new Dataset();
+
+            List<DateTime> dates = new List<DateTime>();
+            List<decimal> prices = new List<decimal>();
+
+            DateTime date = new DateTime(2014, 8, 1, 0, 0, 0);
+            decimal price = 1.5M;
+            for (int counter = 0; counter < 20; counter++)
+            {
+                dates.Add(date);
+                prices.Add(price);
+
+                date = date.AddMinutes(1);
+                price += 0.01M;
+            }
+
+            reliableDataset.load(dates, prices);
+        }
+
+        [TestMethod]
+        public void ShowAllReliableEntriesTest()
+        {
+            foreach (Entry entry in reliableDataset.getEntries())
+            {
+                Console.WriteLine(entry);
+            }
+        }
+
+        [TestMethod]
+        public void basicCalculationTest()
+        {
+            Calculation calulation = new Calculation(reliableDataset);
+            Dataset result = calulation.calculateAllMovingAverages(10);
+
+            // FIXME Implement interpolation and extrapolation so that these tests pass.
+            // Assert.AreEqual(1.545M, result.getEntries().First().Price);
+            Assert.AreEqual(1.645M, result.getEntries().Last().Price);
+        }
+
+        //TODO Split this out into multiple tests.
+        [TestMethod]
+        public void checkBasicCases()
+        {
+            reliableDataset = new Dataset();
+            reliableDataset.load("MovingAveragesData.csv");
+
+            Calculation calculation = new Calculation(reliableDataset);
+            Dataset results = calculation.calculateAllMovingAverages(10);
+
+            // Nothing preceding
+            Assert.AreEqual(1.33877M, results.getEntries().First().Price);
+
+            // Evenly spaced data
+            Assert.AreEqual(1.341999M, results.getEntries().ElementAt(2840).Price);
+
+            // Evenly spaced and across 2 days
+            Assert.AreEqual(1.342014M, results.getEntries().ElementAt(2842).Price);
+
+            // Unevenly spaced data (i.e., 23:46, 23:48, 23:49...)
+            Assert.AreEqual(1.34203125M, results.getEntries().ElementAt(2832).Price);
+        }
+    }
+}
