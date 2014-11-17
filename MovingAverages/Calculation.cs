ba3f6d171eb8cc12327547b4a967313277a03b1d
jonathan_sterling <jonathan.sterling@jagex.com>
2014-11-17 17:35:33 +0000

    [3] Fixed interpolation implementation in Calculation.

--- MovingAverages/Calculation.cs
+++ MovingAverages/Calculation.cs
@@ -10,7 +10,7 @@ namespace MovingAverages
     {
         protected Dataset dataset;
 
-        public Calculation( Dataset dataset )
+        public Calculation(Dataset dataset)
         {
             this.dataset = dataset;
         }
@@ -20,7 +20,7 @@ namespace MovingAverages
         // </summary>
         // <param name="minute">Period of the moving average, in minutes</param>
         // <returns>A dataset showing the calculated moving average over time</returns>
-        public Dataset calculateAllMovingAverages( int minute )
+        public Dataset calculateAllMovingAverages(int minute)
         {
             Dataset results = new Dataset();
 
@@ -44,8 +44,6 @@ namespace MovingAverages
 
         private decimal calculateSingleAverageGivenDateTime(DateTime endTime, int minute)
         {
-            List<Entry> entries = dataset.getEntries();
-
             DateTime startTime = endTime.Subtract(new TimeSpan(00, (minute), 00));
             int endIndex = dataset.getIndexGivenDateTime(endTime);
 
@@ -54,10 +52,10 @@ namespace MovingAverages
 
             while (endIndex >= 0)
             {
-                if (entries.ElementAt(endIndex).Date > startTime)
+                if (dataset.getEntries().ElementAt(endIndex).Date > startTime)
                 {
-                    dates.Add(entries.ElementAt(endIndex).Date);
-                    prices.Add(entries.ElementAt(endIndex).Price);
+                    dates.Add(dataset.getEntries().ElementAt(endIndex).Date);
+                    prices.Add(dataset.getEntries().ElementAt(endIndex).Price);
                     endIndex--;
                 }
                 else
@@ -73,45 +71,38 @@ namespace MovingAverages
             }
             else
             {
-                maxPossibleNumOfEntries = endIndex;   
+                maxPossibleNumOfEntries = endIndex;
             }
             return calculateAveragePrice(dates, prices, maxPossibleNumOfEntries);
         }
 
         private decimal calculateAveragePrice(List<DateTime> dates, List<decimal> prices, int maxPossibleNumOfEntries)
         {
-            List<Entry> pricesAndDates = new List<Entry>();
-
-            if (prices.Count < maxPossibleNumOfEntries)
-            {
-                pricesAndDates = interpolate(dates, prices, maxPossibleNumOfEntries);
-                prices = new List<decimal>();
-
-                foreach (Entry entry in pricesAndDates)
-                {
-                    prices.Add(entry.Price);
-                }
-            }
-
-            decimal sum = 0;
-            
-            foreach(decimal data in prices){
-                sum += data;
-            }
-
             if (prices.Count == 0)
             {
                 return -1;
             }
-            else
+
+            if (prices.Count < maxPossibleNumOfEntries)
             {
-                return sum / ((decimal)prices.Count);
+                prices = interpolate(dates, prices, maxPossibleNumOfEntries);
             }
+
+            decimal sum = 0;
+
+            foreach (decimal data in prices)
+            {
+                sum += data;
+            }
+
+
+            return sum / ((decimal)prices.Count);
+
         }
 
-        private List<Entry> interpolate(List<DateTime> dates, List<decimal> prices, int maxPossibleNumOfEntries)
+        private List<decimal> interpolate(List<DateTime> dates, List<decimal> prices, int maxPossibleNumOfEntries)
         {
-            if (dates.ElementAt(0).Equals(new DateTime(2014, 08, 01, 00, 31, 00)))
+            if (dates.ElementAt(0).Equals(new DateTime(2014, 08, 04, 23, 48, 00)))
             {
                 // Find me debugger;
                 int asfd = 0;
@@ -119,38 +110,33 @@ namespace MovingAverages
 
             decimal[] indicesNeeded = new decimal[maxPossibleNumOfEntries - prices.Count];
             int numOfFoundNeededIndices = 0;
-            decimal[] xValues = new decimal[dates.Count];
-            decimal[] yValues = new decimal[dates.Count];
+
+            decimal[] xValues = new decimal[maxPossibleNumOfEntries];
+            decimal[] yValues = new decimal[maxPossibleNumOfEntries];
 
             // Find the indices that we need to fill
             for (int i = 0; i < dates.Count - 1; i++)
             {
                 if (dates.ElementAt(i).Subtract(dates.ElementAt(i + 1)).Minutes > 1)
                 {
-                    indicesNeeded[numOfFoundNeededIndices] = ((decimal)i + ((decimal)i + 1m)) / 2.0m;
-                    numOfFoundNeededIndices++;
-                }        
+                    indicesNeeded[numOfFoundNeededIndices] = i;
+                }
                 xValues[i] = i;
-                yValues[i] = prices[i];
+                yValues[i] = prices.ElementAt(i);
             }
-            
+
             // Insert those values into our price and date list.
             for (int j = prices.Count - 1; j > 0; j--)
             {
-                if(indicesNeeded.Contains((decimal)j - 0.5m)){
-                    prices.Insert(j, LagrangeInterpolater
-                        .getInterpolatedValueGivenCurrentValuesAndIndexToFind(xValues, yValues, (decimal)j - 0.5m));    
-                    dates.Insert(j, dates.ElementAt(j).Subtract(new TimeSpan(0, 1, 0)));
+                if (indicesNeeded.Contains(j))
+                {
+                    prices.Insert(j, LagrangeInterpolater.getInterpolatedValueGivenCurrentValuesAndIndexToFind(xValues, yValues, j));
+                    dates.Insert(j, dates.ElementAt(j).Add(new TimeSpan(0, 1, 0)));
                     j--;
                 }
             }
 
-            List<Entry> interpolatedList = new List<Entry>();
-            for(int k = 0; k < prices.Count; k++){
-                interpolatedList.Add(new Entry(dates.ElementAt(k), prices.ElementAt(k)));
-            }
-
-            return interpolatedList;
+            return prices;
         }
     }
 }
